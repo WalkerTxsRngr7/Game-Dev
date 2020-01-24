2b930a87ef5751a338dbc9ec32f6269e6a903896
Walker <walkergross7@gmail.com>
2020-01-24 10:04:12 -0600

    Scoring and Basket on camera

--- Apple Picker/Apple Picker/Assets/AppleTree.cs
+++ Apple Picker/Apple Picker/Assets/AppleTree.cs
@@ -3,14 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public GameObject applePrefab;
+    //speed of the apple tree moves
+    public float speed = 1f;
+    //distance where the apple tree turns around
+    public float leftAndRightEdge = 10f;
+    //chance the tree will change directions
+    public float chanceToChangeDirections = 0.1f;
+    //rate at which the apples will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        //drop apples every second
+        Invoke("DropApple", 2f);
+
 	}
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        //basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        //change direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        } else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+        } 
 	}
+
+    private void FixedUpdate()
+    {
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1;
+        }
+    }
 }
