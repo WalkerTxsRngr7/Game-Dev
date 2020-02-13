b872426770ce8e8b329c1f6752d3461a49291562
WalkerTxsRngr7 <walkergross7@gmail.com>
2020-02-13 16:49:33 -0600

    Spawn rock and set up and throw.

--- Block Cannon/Assets/Game.cs
+++ Block Cannon/Assets/Game.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Game : MonoBehaviour {
-    public Text scoreGT;
-    public GameObject rockYellowPrefab;
-    public GameObject rockRedPrefab;
-    //default speed of the rock
-    public float speed = 1f;
-
+    //public Text scoreGT;
+    public GameObject rockYPrefab;
+    public GameObject rockRPrefab;
+    public Camera camMain;
+    public Camera camCircle;
+    public static bool yPrev = false;
 
     // Use this for initialization
     void Start () {
-        GameObject rockYellow = Instantiate<GameObject>(rockYellowPrefab);
-        
-	    
-	}
+        camMain.enabled = true;
+        camCircle.enabled = false;
+        SpawnRock();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown("View"))
+        {
+            camMain.enabled = !camMain.enabled;
+            camCircle.enabled = !camCircle.enabled;
+        }
 	}
+
+    //Spawn rock to throw, alternating teams
+    public void SpawnRock()
+    {
+        if (yPrev)
+        {
+            GameObject rockRed = Instantiate<GameObject>(rockRPrefab);
+            yPrev = false;
+        }
+        else
+        {
+            GameObject rockYellow = Instantiate<GameObject>(rockYPrefab);
+            yPrev = true;
+        }
+        
+    }
 }
--- Block Cannon/Assets/Rock.cs
+++ Block Cannon/Assets/Rock.cs
@@ -3,14 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rock : MonoBehaviour {
+    public Rigidbody rb;
+    public bool thrown = false;
+    //default speed of the rock
+    public float speed = 1f;
+    public float aimSpeed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    public float throwForce = 20f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+        float actualSpeed = rb.velocity.magnitude;
+
+        if (!thrown)
+        {
+            // Get the horizontal and vertical axis.
+            // By default they are mapped to the arrow keys.
+            // The value is in the range -1 to 1
+            float rotation = Input.GetAxis("Rotate") * rotationSpeed;
+            float translation = Input.GetAxis("Horizontal") * aimSpeed;
+            float power = Input.GetAxis("Vertical") * 2f;
+
+            // Make it move 10 meters per second instead of 10 meters per frame...
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
+            power *= Time.deltaTime;
+
+            // Rotate around our y-axis
+            transform.Rotate(0, 0, rotation);
+
+            // Move translation along the object's z-axis
+            transform.Translate(translation, 0, 0);
+
+            // Change power
+            throwForce += power;
+
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                thrown = true;
+                Throw();
+                
+            }
+        }
+        else if (actualSpeed < 0.5)
+        {
+            //rb.velocity = new Vector3(0, 0, 0);
+            //Or
+            //rb.constraints = RigidbodyConstraints.FreezeAll;
+            SpawnRock();
+        }
+
+        
+    }
+
+    void Throw()
+    {
+        Rigidbody rb = this.GetComponent<Rigidbody>();
+        rb.AddForce(transform.up * throwForce, ForceMode.Impulse);
+    }
+
 }
