b003f07b83d51c8456985d3b538673dae89b6806
WalkerTxsRngr7 <43824854+WalkerTxsRngr7@users.noreply.github.com>
2020-02-13 10:04:51 -0600

    Finished rocks and started main code

---
+++ Block Cannon/Assets/Game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+    public Text scoreGT;
+    public GameObject rockYellowPrefab;
+    public GameObject rockRedPrefab;
+    //default speed of the rock
+    public float speed = 1f;
+
+
+    // Use this for initialization
+    void Start () {
+        GameObject rockYellow = Instantiate<GameObject>(rockYellowPrefab);
+        
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
