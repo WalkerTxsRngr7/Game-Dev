10109f91eaecf3b0dc87f3725b7b7c05d3f7bc29
WalkerTxsRngr7 <43824854+WalkerTxsRngr7@users.noreply.github.com>
2020-03-06 10:12:21 -0600

    Added attack animation and skeletos animation

--- Dungeon Delver/Assets/__Scripts/Dray.cs
+++ Dungeon Delver/Assets/__Scripts/Dray.cs
@@ -4,44 +4,94 @@ using UnityEngine;
 
 public class Dray : MonoBehaviour
 {
+    public enum eMode {idle, move, attack, transition}
+
     [Header("Set in Inspector")]
     public float speed = 5;
+    public float attackDuration = 0.25f;
+    public float attackDelay = 0.5f;
 
     [Header("Set Dynamically")]
     public int dirHeld = -1;        //direction held by user of movement key
-    private Rigidbody rigid;
+    public int facing = 1;          //direction Dray is facing
+    public eMode mode = eMode.idle;
 
+
+    private float timeAtkDone = 0;
+    private float timeAtkNext = 0;
+
+    private Rigidbody rigid;
+    private Animator anim;
+
+    private Vector3[] directions = new Vector3[] {Vector3.right, Vector3.up, Vector3.left, Vector3.down};
+
+    private KeyCode[] keys = new KeyCode[]{KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow};
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         dirHeld = -1;
-        if (Input.GetKey(KeyCode.RightArrow)) dirHeld = 0;
-        if (Input.GetKey(KeyCode.UpArrow)) dirHeld = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) dirHeld = 2;
-        if (Input.GetKey(KeyCode.DownArrow)) dirHeld = 3;
 
-        Vector3 vel = Vector3.zero;
-        switch (dirHeld)
-        {
-            case 0:
-                vel = Vector3.right;
+        for (int i = 0; i < 4; i++){
+            if (Input.GetKey(keys[i])) dirHeld = i;
+        }
+        //press the attack button
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= timeAtkNext){
+            mode = eMode.attack;
+            timeAtkDone = Time.time + attackDuration;
+            timeAtkNext = Time.time;
+        }
+
+        //finish our attack when it's over
+        if (Time.time >= timeAtkDone){
+            mode = eMode.idle;
+        }
+
+        //choosing the proper mode if we're not attacking
+        if (mode != eMode.attack) {
+            if (dirHeld == -1) {
+                mode = eMode.idle;
+            }
+            else {
+                facing = dirHeld;
+                mode = eMode.move;
+            }
+        }
+
+        //act on the current mode
+        switch (mode) {
+            case eMode.attack:
+                anim.CrossFade("Dray_Attack_" + facing, 0);
+                anim.speed = 0;
                 break;
-            case 1:
-                vel = Vector3.up;
+            case eMode.idle:
+                anim.CrossFade("Dray_Walk_" + facing, 0);
+                anim.speed = 0;
                 break;
-            case 2:
-                vel = Vector3.left;
-                break;
-            case 3:
-                vel = Vector3.down;
+            case eMode.move:
+                anim.CrossFade("Dray_Walk_" + facing, 0);
+                anim.speed = 1;
                 break;
         }
 
+        Vector3 vel = Vector3.zero;
+        
+        if (dirHeld > -1) vel = directions[dirHeld];
         rigid.velocity = vel * speed;
+
+        //animation
+        if (dirHeld == -1)
+        {
+            anim.speed = 0;
+        } else
+        {
+            anim.CrossFade("Dray_Walk_" + dirHeld, 0);
+            anim.speed = 1;
+        }
     }
 }
---
+++ Dungeon Delver/Assets/__Scripts/SwordController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    private GameObject sword;
+    private Dray dray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sword = transform.Find("Swords_0").gameObject;
+        dray = transform.parent.GetComponent<Dray>();
+        //deactivate the sword
+        sword.SetActive(false);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 90 * dray.facing);
+        sword.SetActive(dray.mode == Dray.eMode.attack);
+    }
+}
