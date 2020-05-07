b802ce1647e888cb1a91e7e917f112a416e6aa81
WalkerTxsRngr7 <walkergross7@gmail.com>
2020-05-07 09:15:59 -0400

    Reaper - Updated Scripts

---
+++ Reaper - Updated/Assets/Scripts/Reaper.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reaper : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Rigidbody scythe;
+
+    public bool thrown = false;
+    public float throwForce = 10f;
+    public Transform throwPoint;
+    public Camera cam;
+    Vector3 mousePos;
+    private bool m_FacingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+        if (Input.GetButtonDown("Fire1") && thrown == false)    //throw scythe
+        {
+            Throw();
+        }
+        else if (Input.GetButtonDown("Fire2") && thrown == true)    //teleport to scythe
+        {
+            rb.transform.position = scythe.transform.position;
+            thrown = false;
+        }
+        else if (thrown == false)   //hold scythe
+        {
+            scythe.transform.position = throwPoint.position;
+            scythe.transform.rotation = rb.rotation;
+        }
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        //change direction of Reaper depending on mouse position
+        if (mousePos.x < rb.position.x && m_FacingRight != false) //facing left
+        {
+            Flip();
+        }
+        else if (mousePos.x >= rb.position.x && m_FacingRight != true) //facing right
+        {
+            Flip();
+        }
+
+        Vector2 lookDir = mousePos - rb.position;
+
+        //change angle of throwpoint for scythe
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        throwPoint.transform.rotation = Quaternion.Euler(0,0,angle);
+    }
+
+    //flip Reaper to face right or left
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+
+    //throw the scythe
+    void Throw()
+    {
+        //GameObject scythe = Instantiate(scythePrefab, throwPoint.position, throwPoint.rotation);
+        Rigidbody rb = scythe.GetComponent<Rigidbody>();
+        rb.AddForce(throwPoint.right * throwForce, ForceMode.Impulse);
+        thrown = true;
+    }
+}
---
+++ Reaper - Updated/Assets/Scripts/Scythe.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scythe : MonoBehaviour
+{
+    public int enemiesLeft = 0;
+    public Text timerBox;
+    public Text endTimerBox;
+    private bool timerActive = true;
+
+    private float timer = 0.0f;
+
+    public bool TimerActive { get => timerActive;}
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemiesLeft = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        endTimerBox.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerActive)
+        {
+            timer += Time.deltaTime;
+            timerBox.text = timer.ToString("F2");
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        //find out what hit the scythe hit
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Enemy")
+        {
+            Destroy(collidedWith);
+            enemiesLeft--;
+            if (enemiesLeft <= 0)
+            {
+                timerActive = false;
+                timerBox.enabled = false;
+                endTimerBox.enabled = true;
+                endTimerBox.text = timer.ToString("F2") + " seconds";
+            }
+        }
+    }
+}
